9ce99ff571ec642389b05a171f2b0f4fcdedd083
Somgupta786 <somvishwari786@gmail.com>
2024-01-09 02:56:05 +0530

    add routing in Signup Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 "use client"
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-export default function Home() {
-  const router = useRouter()
-  router.push("/signup")
-  return (
-  <div>
- 
-  </div>
-  )
-}
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigaton';
+
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    
+    router.push('/signup');
+  }, []);
+  
+
+  return <div>Redirecting...</div>;
+};
+
+export default Home;
